9f59a94fc06ce3cb02efc6bb0ffcc01c0e06aa40
hhh <157157135983@163.com>
2020-10-21 16:34:34 +0800

    cookie 处理工具

---
+++ src/components/cookie/index.js
@@ -0,0 +1,19 @@
+
+/**
+ *  设置cookie
+ * @param {*} key
+ * @param {*} val 
+ */
+export const setCookie = (key, val) => {
+
+
+};
+
+/**
+ * cookie取值
+ * @param {*} key 
+ */
+export const getCookie = (key) => {
+
+
+};
